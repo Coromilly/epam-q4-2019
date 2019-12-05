8a1a0c73014ec5518113d5e768d6e842cd658f28
Aleksandr Levdansky <coromilly@gmail.com>
2019-12-06 00:14:06 +0300

    Add some logic to test.py. Add docstrings.

--- rss_reader.py
+++ rss_reader.py
@@ -3,25 +3,52 @@ from bs4 import BeautifulSoup
 import feedparser
 import json
 import logging
+import sys
 
 
 class Content:
+    """Determining functions to create empty entry(news object) and output it."""
 
     def __init__(self):
-        """Creating empty entry objects."""
+        """Creating empty entry object."""
         self.title = ''
         self.date = ''
         self.link = ''
         self.content = ''
 
     def output(self):
+        """Print entry to stdout."""
         print('\n' + 'Title: ' + self.title)
         print('Publication Date: ' + self.date)
         print('Link: ' + self.link + '\n')
         print(self.content)
 
 
+def logger(args):
+    """Creating script_logger with 2 handlers.
+
+    verbose_handler has level 'DEBUG' and prints all logs in stdout if --verbose argument is provided.
+    standard_handler has level 'DEBUG' and prints only important logs.
+    """
+    script_logger = logging.getLogger('rss_logger')
+    script_logger.setLevel(logging.DEBUG)
+    standard_handler = logging.StreamHandler()
+    standard_handler.setLevel(logging.WARNING)
+    standard_formatter = logging.Formatter('%(asctime)s - %(message)s')
+    standard_handler.setFormatter(standard_formatter)
+    verbose_handler = logging.StreamHandler()
+    verbose_handler.setLevel(logging.DEBUG)
+    verbose_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    verbose_handler.setFormatter(verbose_formatter)
+    if args.verbose:
+        script_logger.addHandler(verbose_handler)
+    else:
+        script_logger.addHandler(standard_handler)
+    return script_logger
+
+
 def getting_arguments():
+    """Parsing arguments from command line."""
     parser = argparse.ArgumentParser(description='Pure Python command-line RSS reader.', add_help=True)
     parser.add_argument('source', type=str, help='RSS URL')
     parser.add_argument('--version', action='store_true', help='Print version info')
@@ -32,13 +59,17 @@ def getting_arguments():
     return args
 
 
-def getting_feed(args):
+def getting_feed(args, script_logger):
+    """Simply getting unreadable feed from URL specified in 'source'."""
+    script_logger.info('Getting feed.')
     thefeed = feedparser.parse(args.source)
+    script_logger.info('Feed was get successfully.')
     return thefeed
 
 
-def creating_list_of_entries(thefeed):
-    """Creating list of entry objects with readable news"""
+def creating_news_list(thefeed, script_logger):
+    """Creating list of entry objects with readable news."""
+    script_logger.info('Creating list of news entries.')
     entries = thefeed.entries
     news_list = []
     for entry in entries:
@@ -48,32 +79,42 @@ def creating_list_of_entries(thefeed):
         try:
             news.date = str(BeautifulSoup(entry.published, 'html.parser'))
         except AttributeError:
+            script_logger.warning('Current article has no date.')
             news.date = 'Date: this article has no date.'
         try:
             news.link = str(entry.link)
         except AttributeError:
+            script_logger.warning('Current article has no link.')
             news.link = 'Link: this article has no link.'
-        content = BeautifulSoup(entry.summary, 'html.parser')
-        news.content = content.text
+        try:
+            content = BeautifulSoup(entry.summary, 'html.parser')
+            news.content = content.text
+        except AttributeError:
+            script_logger.warning('Current article has no content.')
+            news.content = 'Content: this article has no content.'
         news_list.append(news)
+    script_logger.info('List of news was created successfully.')
     return news_list
 
 
-def output(args, thefeed, news_list):
-    """Outputing news"""
+def limit_news_list(news_list, args, script_logger):
+    script_logger.info('Creating limit list of news entries.')
+    limit = args.limit
+    news_list = news_list[:limit]
+    script_logger.info('Limit list of news was created successfully.')
+    return news_list
+
+
+def output(news_list, thefeed, script_logger):
+    """Output news."""
+    script_logger.info('Please, read news.')
     print('Feed: ', thefeed.feed.get('title', ''))
-    if args.limit != 0:
-        limit = args.limit
-        for news in news_list[:limit]:
-            news.output()
-    elif args.limit == 0:
-        print('Limit = 0, no news to print.')
-    else:
-        for news in news_list:
-            news.output()
+    for news in news_list:
+        news.output()
 
 
-def convert_to_json(news_list, args, thefeed):
+def output_in_json(news_list, thefeed, script_logger):
+    """Output news in json format."""
     news_list_json = []
     feed = thefeed.feed.get('title', '')
     for news in news_list:
@@ -88,28 +129,13 @@ def convert_to_json(news_list, args, thefeed):
             }
         }
         news_list_json.append(news_dict)
-    if args.limit:
-        limit = args.limit
-        for news in news_list_json[:limit]:
-            print(json.dumps(news, indent=4, ensure_ascii=False))
-    elif args.limit == 0:
-        print('Limit = 0, no news to print.')
-    else:
-        for news in news_list_json:
-            print(news)
+    script_logger.info('Please, read news in json format.')
+    for news in news_list_json:
+        print(json.dumps(news, indent=4, ensure_ascii=False))
 
 
-def printing_version(args):
-    if args.version:
-        print('rss_reader, version 1.0')
-
-
-def logger():
-    script_logger = logging.getLogger('rss_logger')
-    script_logger.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    ch.setFormatter(formatter)
-    script_logger.addHandler(ch)
-    return script_logger
+def print_version(script_logger):
+    """Simply prints version of rss_reader.py script."""
+    script_logger.info('Check program version.')
+    print('rss_reader, version 1.0')
+    sys.exit()
--- test.py
+++ test.py
@@ -3,11 +3,20 @@ from rss_reader import *
 
 def main():
     args = getting_arguments()
-    thefeed = getting_feed(args)
-    news_list = creating_list_of_entries(thefeed)
-    output(args, thefeed, news_list)
-    convert_to_json(news_list, args, thefeed)
-    printing_version(args)
+    script_logger = logger(args)
+    if args.version:
+        print_version(script_logger)
+    else:
+        thefeed = getting_feed(args, script_logger)
+        news_list = creating_news_list(thefeed, script_logger)
+        if args.limit:
+            news_lst = limit_news_list(news_list, args, script_logger)
+            if args.json:
+                output_in_json(news_lst, thefeed, script_logger)
+            else:
+                output(news_lst, thefeed, script_logger)
+        else:
+            output(news_list, thefeed, script_logger)
 
 
 if __name__ == '__main__':
